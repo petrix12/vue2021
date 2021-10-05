2c89e7a173db59775d95efc7238d786499e0e66e
Pedro Bazó <bazo.pedro@gmail.com>
2021-10-05 17:55:29 -0400

    Commit 081: Pruebas sobre Fetch Api

--- 03fundamentos/tests/unit/indecision.spec.js
+++ 03fundamentos/tests/unit/indecision.spec.js
@@ -5,7 +5,13 @@ describe('Indecision Component', () => {
     let wrapper
     let clgSpy
 
-    global.fetch = jest.fn()
+    global.fetch = jest.fn( () => Promise.resolve({
+        json: () => Promise.resolve({
+            answer: 'yes',
+            forced: false,
+            image:  'https://yesno.wtf/assets/yes/2.gif'
+        })
+    }))
 
     beforeEach(() => {
         wrapper = shallowMount(Indecision)
@@ -32,11 +38,18 @@ describe('Indecision Component', () => {
         const input = wrapper.find('input')
         await input.setValue('Hola Mundo?')
 
-        expect(getAnswerSpy).toHaveBeenCalledTimes(1)
+        expect(clgSpy).toHaveBeenCalledTimes(1)
+        expect(getAnswerSpy).toHaveBeenCalled()
     })
 
-    test('pruebas en getAnswer', () => {
+    test('pruebas en getAnswer', async() => {
+        await wrapper.vm.getAnswer()
 
+        const img = wrapper.find('img')
+
+        expect( img.exists() ).toBeTruthy()
+        expect( wrapper.vm.img ).toBe('https://yesno.wtf/assets/yes/2.gif')
+        expect( wrapper.vm.answer ).toBe('Si!')
     })
 
     test('pruebas en getAnswer - Fallo en el API', () => {
