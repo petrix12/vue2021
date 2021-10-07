2ba1c7cfaf6d2f39f4bfddf74539e5a7ba161d33
Pedro Bazó <bazo.pedro@gmail.com>
2021-10-07 08:52:27 -0400

    Commit 092: Obtener nombres de los 4 pokémons

--- 04pokemon/codigo fuente autor/vue-pokemon-game-main/src/helpers/getPokemonOptions.js
+++ 04pokemon/codigo fuente autor/vue-pokemon-game-main/src/helpers/getPokemonOptions.js
@@ -1,17 +1,27 @@
 import pokemonApi from '../api/pokemonApi'
 
 const getPokemons = () => {
+
     const pokemonsArr = Array.from( Array(650) )
+    
     return pokemonsArr.map( ( _ , index ) => index + 1 )
+
 }
 
+
 const getPokemonOptions = async() => {
-    const mixedPokemons = getPokemons().sort( () => Math.random() - 0.5 ) 
+
+    const mixedPokemons = getPokemons()
+                            .sort( () => Math.random() - 0.5 )
+    
     const pokemons = await getPokemonNames( mixedPokemons.splice(0,4) )
+
     return pokemons
+
 }
 
 const getPokemonNames = async( [a,b,c,d] = [] ) => {
+
     // const resp = await pokemonApi.get(`/3`)
     // console.log(resp.data.name, resp.data.id)
     const promiseArr = [
@@ -21,6 +31,7 @@ const getPokemonNames = async( [a,b,c,d] = [] ) => {
         pokemonApi.get(`/${ d }`),
     ]
 
+
     const [ p1, p2, p3, p4 ] = await Promise.all( promiseArr )
     
     return [
@@ -29,6 +40,12 @@ const getPokemonNames = async( [a,b,c,d] = [] ) => {
         { name: p4.data.name, id: p3.data.id },
         { name: p3.data.name, id: p4.data.id },
     ]
+
 }
 
+
+
 export default getPokemonOptions
+
+
+
